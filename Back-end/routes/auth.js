11f47defcd7a154a252fea0cee67253bca0ab55b
pizqm <75383534+pizqm@users.noreply.github.com>
2024-02-22 14:11:44 +0100

    Update auth.js
    Añadido rutas para la comprobación de contraseña

--- Back-end/routes/auth.js
+++ Back-end/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const { registerCtrl, loginCtrl, codeVerification } = require("../controllers/auth")
-const { validatorRegister, validatorLogin, validatorCode } = require("../validators/auth")
+const { registerCtrl, loginCtrl, codeVerification, newPassword, passwordCode, passwordEmail } = require("../controllers/auth")
+const { validatorRegister, validatorLogin, validatorCode, validatorPassword, validatorCode2, validatorEmail } = require("../validators/auth")
 const router = express.Router()
 
 router.post("/login", validatorLogin, loginCtrl)
@@ -8,5 +8,11 @@ router.post("/login", validatorLogin, loginCtrl)
 router.post("/register", validatorRegister, registerCtrl)
 
 router.post("/code", validatorCode, codeVerification)
+
+router.post("/password/password", validatorPassword, newPassword)
+
+router.post("/password/code", validatorCode2, passwordCode)
+
+router.post("/password/email", validatorEmail, passwordEmail)
 
 module.exports = router
