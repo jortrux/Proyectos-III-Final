51b72436a654376c1b7e0346e8d569598590fc7b
pizqm <75383534+pizqm@users.noreply.github.com>
2024-02-22 14:17:11 +0100

    Update models/index.js
    Añadido el modelo de la tabla tokens para comprobación de los tokens validos despues de cambiar de contraseña

--- Back-end/models/index.js
+++ Back-end/models/index.js
@@ -1,11 +1,14 @@
 // Import all your model files here
 const User = require('./mongodb/users');
 const Post = require('./mongodb/post');
+const Tokens = require('./mongodb/token');
 // Add more models as needed
 
 // Export all the models as an object
 module.exports = {
   usersModel: User,
   postModel: Post,
+  tokenModel: Tokens,
+
   // Add more models as needed
 };
