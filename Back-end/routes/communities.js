985bdb3d591f732d0bc62b1674a939556df0bb8d
Dario Llodra <60240718+nilerdar@users.noreply.github.com>
2024-03-07 18:46:35 +0100

    Update communities.js

--- Back-end/routes/communities.js
+++ Back-end/routes/communities.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const {authMiddleware} = require("../middleware/session")
 const {getCommunities, getUserCommunities, getCommunity, createCommunity, deleteCommunity, updateCommunity} = require("../controllers/user")
-const {validatorCreateComunity, validatorGetComunity} = require("../validators/communities")
+const {validatorCreateComunity, validatorId } = require("../validators/communities")
 const router = express.Router()
 
 /* TODO
@@ -12,14 +12,14 @@ Salir de una comunidad
 
 router.get("/getItems", authMiddleware, getCommunities)
 
-router.get("/getItem/:id", authMiddleware, validatorGetComunity, getCommunity)
+router.get("/getItem/:id", authMiddleware, validatorId , getCommunity)
 
 router.get("/user", authMiddleware, getUserCommunities)
 
 router.post("/createItem", authMiddleware, validatorCreateComunity, createCommunity)
 
-router.delete("/deleteItem/:id", authMiddleware, validatorGetComunity, deleteCommunity);
+router.delete("/deleteItem/:id", authMiddleware, validatorId , deleteCommunity);
 
-router.put("/updateItem/:id", authMiddleware, validatorGetComunity, validatorCreateComunity, updateCommunity)
+router.put("/updateItem/:id", authMiddleware, validatorId , validatorCreateComunity, updateCommunity)
 
 module.exports = router
