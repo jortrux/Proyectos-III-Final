422c5b1ca60bc7f66fe23579e7a439c71e6ac9e1
Dario Llodra <60240718+nilerdar@users.noreply.github.com>
2024-03-07 18:45:38 +0100

    Update communities.js

--- Back-end/validators/communities.js
+++ Back-end/validators/communities.js
@@ -5,24 +5,21 @@ const validatorCreateComunity = [
     check("name").exists().notEmpty(),
     check("description").exists().notEmpty(),
     check("createdBy").exists().notEmpty().isMongoId(),
-    check("createdAt").exists().notEmpty(),
     check("members").exists().notEmpty().isMongoId(),
     check("public").exists().notEmpty(),
     check("topics").exists().notEmpty(),
-    check("avatar").exists().notEmpty(),
     check("avatar.filename").exists().notEmpty(),
     check("avatar.url").exists().notEmpty(),
-    check("settings").exists().notEmpty(),
     check("settings.allowPosts").exists().notEmpty(),
     (req, res, next) => {
         return validateResults(req, res, next)
     }
 ]
 
-const validatorGetComunity = [
+const validatorId = [
     check("id").exists().notEmpty().isMongoId(),
     (req, res, next) => {
         return validateResults(req, res, next)
     }
 ]
-module.exports = { validatorCreateComunity,  }    
+module.exports = { validatorCreateComunity, validatorId }    
