4d658473121e1f600febada59a0b973f7d0abf9c
Dario Llodra <60240718+nilerdar@users.noreply.github.com>
2024-04-30 15:27:10 +0200

    Update page.js

--- Front-end/src/app/page.js
+++ Front-end/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Navbar from "../components/NavbarDARIO";
+import Navbar from "../components/Navbar";
 import ProjectsSection from "../components/ProjectsSection";
 import Link from "next/link";
 
