56728f54a1068abc43a0fa2501bbaac9fc58ae46
Samcer8 <samcerbl@gmail.com>
2024-05-10 01:05:36 +0200

    Update Home

--- Front-end/src/app/home/page.js
+++ Front-end/src/app/home/page.js
@@ -1,107 +1,89 @@
 'use client'
 
-import Image from "next/image";
-import styles from "../page.module.css";
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import Link from "next/link";
-import { useState, useRef, useEffect } from 'react'
-import backArrow from "../../resources/images/backArrow.png"
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import foto from "../../resources/images/gentehablando.png"
+import nextArrow from "../../resources/images/nextArrow.png"
+import ActivitySection from "@/components/ActivitySection"
+import ProjectCard from '@/components/ProjectCard';
+import masGente from '../../resources/images/masGente.png'
 
-export default function Code() {
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const user = {
-      email: email,
-      code: code
+function Home() {
+    var registered = true;
+    var name = "Elisa";
+  
+    const handleSubmit = (e) => {
+      
     }
-
-    console.log(user);
-    fetch("http://87.221.139.203:443/api/auth/password/code", {
-                method: "POST",
-                headers: {
-                
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user)
-            })
-                .then((res) => res.json())
-                .then((data) => console.log(data))
-                .then(router.push("../forgot/code"))
-  }
-
-  const [codes, setCodes] = useState(Array(6).fill(''));
-  const inputRefs = useRef([]);
-
-  useEffect(() => {
-    inputRefs.current[0].focus();
-  }, []);
-
-  const handleChange = (index, e) => {
-    const value = e.target.value;
-    setCodes(prevCodes => {
-      const newCodes = [...prevCodes];
-      newCodes[index] = value;
-      return newCodes;
-    });
-    if (value !== '' && index < codes.length - 1) {
-      inputRefs.current[index + 1].focus();
-    }
-    if (value === '' && index > 0) {
-      inputRefs.current[index - 1].focus();
-    }
-  };
-
-  const handleKeyDown = (index, e) => {
-    if (e.key === 'Backspace' && index > 0 && codes[index] === '') {
-      inputRefs.current[index - 1].focus();
-    }
-  };
-
-  return (
-    <main className="md:bg-gray-200 md:w-screen md:h-screen md:flex md:justify-center">
-      <div className="bg-white md:rounded-xl md:h-auto flex-col self-center justify-center items-center gap-8 flex">
-        <div className="md:m-20 flex-col flex">
-          <div className="flex space-x-4 mx-2 md:mx-0">
-            <Image src={backArrow} width={32} height={32} alt="Esto no furula" className="box-content h-8 w-8 self-center md:hidden"></Image>
-            <h1 className="font-extrabold font-Montserrat text-3xl md:text-5xl">Recuperación de contraseña</h1>
-          </div>
-          <div className="flex gap-3 mt-2 md:justify-center mx-3 md:mx-0">
-            <div className="w-full md:w-72 bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"></div>
-            <div className="w-full md:w-72 bg-blue-600 h-2.5 rounded-full dark:bg-blue-500"></div>
-            <div className="w-full md:w-72 bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700"></div>
-          </div>
-          <div >
-            <p className="font-Montserrat md:mt-4 mx-3 md:mx-0 text-sm text-zinc-500">Introduce el código enviado al correo elisa.rodriguez@live.u-tad.com para recuperar la contraseña.</p>
-          </div>
-          <div className="my-12">
-            <div className="flex justify-center">
-              <div className="flex space-x-2">
-                {codes.map((code, index) => (
-                  <input
-                    key={index}
-                    ref={ref => (inputRefs.current[index] = ref)}
-                    type="text"
-                    maxLength="1"
-                    value={code}
-                    onChange={e => handleChange(index, e)}
-                    onKeyDown={e => handleKeyDown(index, e)}
-                    className="w-12 h-12 md:h-16 text-center text-3xl font-Montserrat text-gray-600 font-bold border-2 border-gray-400 md:border-blue-500 rounded focus:outline-none"
-                  />
-                ))}
+  
+    return (
+      <main className= "min-h-screen bg-[#FFFFFF]">
+        <Navbar/>
+        <div className='md:mt-12'>
+          <div className='mx-3 mt-4'>
+            <div className={registered ? 'flex flex-row space-x-28 md:space-x-96' : 'flex flex-row'}>
+              <p className={registered ? 'text-slate-900 font-extrabold text-3xl font-Montserrat mt-1 md:self-start md:mr-56 md:text-5xl' : 'text-slate-900 font-extrabold text-3xl font-Montserrat'}>{registered ? "¡Hola, "+ name + "!" : "Descubre qué está ocurriendo en U-tad"}</p>
+              <Image src={nextArrow} width={48} height={48} alt="Esto no furula" className={registered ? "box-content mb-4 md:" : "box-content self-center"}></Image>
+            </div>
+            <div className={registered ? "block mb-4" : "hidden"}>
+              <div>
+                <p className='text-slate-900 font-Montserrat font-semibold mb-5 md:text-2xl'>DESCUBRE QUÉ ESTÁ OCURRIENDO EN EL CAMPUS U-TAD</p>
+              </div>
+              <p className='text-blue-600 font-semibold font-Montserrat -mb-0.5 md:text-2xl'>ACTIVIDADES QUE HAS GUARDADO</p>
+              <div className='overflow-x-scroll'>
+                <div className='flex'>
+                  <ProjectCard></ProjectCard>
+                  <ProjectCard></ProjectCard>
+                  <ProjectCard></ProjectCard>
+                </div>
               </div>
             </div>
-            <div className="flex justify-center mt-2">
-                <p className="text-blue-600 underline font-Montserrat">Volver a enviar un código</p>
+            <div>
+              <p className='text-blue-600 font-semibold font-Montserrat -mb-0.5 md:text-2xl'>PRÓXIMAS ACTIVIDADES</p>
+              <div className='overflow-x-scroll'>
+                <div className='flex'>
+                  <ProjectCard></ProjectCard>
+                  <ProjectCard></ProjectCard>
+                  <ProjectCard></ProjectCard>
+                </div>
+              </div>
+            </div>
+            <div className="my-4 md:my-0 md:mx-3">
+              <div className= "flex flex-col md:grid md:grid-cols-2 md:grid-rows-1 md:gap-8">
+                <Image src={foto} width={672} height={374} alt="Esto no furula" className="box-content self-center md:col-start-1"></Image>
+                <div className='md:col-start-2 self-center'>
+                  <p className="text-slate-900 font-semibold font-Montserrat my-2 md:text-2xl">APÚNTATE A ACTIVIDADES Y CREA LAS TUYAS PROPIAS EN U-TAS ACTIVITIES</p>
+                  <ul className='list-disc text-zinc-700 text-sm'>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">En nuestra sección de actividades podrás descubrir los eventos que están por suceder en U-tad.</li>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">Podrás llegar a tantas personas como desees con las opciones de privacidad de las actividades.</li>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">Conoce variedad de actividades desde una quedada para conocer amigos hasta masterclasses con los profesionales de la industria.</li>
+                  </ul>
+                  <button className="bg-blue-600 text-white font-bold font-Montserrat py-3 px-4 rounded-xl md:text-lg md:justify-self-end md:mr-44">CREA UNA ACTIVIDAD</button>
+                </div>
+                
+              </div>
+            </div>
+            <div className=" my-4 md:mx-3">
+              <div className= "flex flex-col md:grid md:grid-cols-2 md:grid-rows-1 md:gap-8">
+                <Image src={masGente} width={672} height={374} alt="Esto no furula" className="box-content self-center md:col-start-2 md:justify-self-end"></Image>
+                <div className='md:col-start-1 md:row-start-1 self-center'>
+                  <p className="text-slate-900 font-semibold font-Montserrat my-2 md:text-2xl">FORMA PARTE DE COMUNIDADES CON TUS MISMOS INTERESES</p>
+                  <ul className='list-disc text-zinc-700 text-sm'>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">Crea foros para compartir tus dudas y gustos con el resto de la comunidad U-tad.</li>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">Encuentra tu lugar dentro de las diversas comunidades correspondientes a todos los ámbitos de la universidad.</li>
+                    <li className="text-zinc-700 text-sm font-Montserrat md:text-lg">Descubre nuevos eventos relacionados a los temas de discusión de los foros.</li>
+                  </ul>
+                  <button className="bg-blue-600 text-white font-bold font-Montserrat py-3 px-4 rounded-xl md:text-lg md:justify-self-end md:mr-44">EXPLORA LAS COMUNIDADES</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="grid justify-items-center grid-cols-1 md:grid-cols-2">
-            <button className="hidden md:block border-opacity-100 border-2 border-blue-500 bg-white text-blue-600 text-xl font-bold font-Montserrat py-3 px-4 rounded-xl md:justify-self-start md:ml-44">VOLVER</button>
-            <button className="bg-blue-600 text-white text-xl font-bold font-Montserrat py-3 px-4 rounded-xl md:justify-self-end md:mr-44">CONTINUAR</button>
-          </div>
         </div>
-        
-      </div>
-    </main>
-  );
-}
+      </main> 
+    )
+  }
+  
+  export default Home;
